177313cd909c5dfd02d3d110ed50fe4a30b03387
Marion Nabulobi <arinabulobi@gmail.com>
2024-11-07 19:04:11 +0300

    add hooks form

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchchange}) {
+  const [category, setCategory] = useState('All');
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+    const newSearchText = e.target.value
+    setSearchText(newSearchText);
+    onSearchchange(newSearchText);
+  }
+
+  const handleCategoryChange = (e) => {
+    const newCategory = e.target.value
+    setCategory(newCategory);
+    onCategoryChange(newCategory);
+  };
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input type="text" name="search" placeholder="Search..." value={searchText} onChange={handleSearchChange} />
+      <select name="filter" value={category} onChange={handleCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
